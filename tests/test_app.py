b81e87dcbbd16ccf7307fe816b7956b4fbac2ecd
tobgays839-cloud <tobgays839@gmail.com>
2026-01-19 23:31:54 +1100

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from app import app
+
+def test_home_page():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
